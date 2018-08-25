db22042082969d1b5b8cc40e1106be796d3da904
Ravi Upadhyay <ravupad@gmail.com>
2018-08-25 09:39:00 +0530

    adding title and better structuring

--- src/main/java/rws/App.java
+++ src/main/java/rws/App.java
@@ -1,8 +1,13 @@
 package rws;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
 import org.jsoup.Jsoup;
@@ -15,26 +20,41 @@ public class App {
 	
 	public static void main(String[] args) 
 			throws IOException {
-		BufferedWriter out = null;
-		FileWriter fstream = new FileWriter("out.html", true);
-		var url = "https://www.wuxiaworld.com/novel/emperors-domination/emperor-chapter-1197";
+		Scanner scanner = null;
+		Writer out = null;
 		try {
-			out = new BufferedWriter(fstream);
-	    while(true) {
+			scanner = new Scanner(System.in);
+			System.out.print("Enter url of first chapter: ");
+			String url = scanner.nextLine();
+			System.out.print("Enter max chapters to download: ");
+			long max = Long.parseLong(scanner.nextLine());
+			out = new BufferedWriter(
+					new OutputStreamWriter(
+							new FileOutputStream("test.html"), 
+							"UTF-8"));
+			String next = null;
+			out.write("<html>\n<head><meta charset='utf-8'></head>\n<body>\n<div class='book'>\n");
+	    while(max > 0) {
 	    	var doc = getDocument(url);
-	    	var body_as_string = getBodyFromWuxia(doc);
-	    	out.write(body_as_string);
-	    	var next = getNextChapterFromWuxia(doc);
-	    	if(next == null) break;
+	    	var title = getTitleFromWuxia(doc);
+	    	var lines = getBodyFromWuxia(doc);
+	    	next = getNextChapterFromWuxia(doc);
 	    	url = wuxia_base + next;
+	    	out.write("<div class='chapter'>\n");
+	    	out.write("<h4>" + title + "</h4>\n");
+	    	for(String line: lines) {
+	    		out.write("<p>" + line + "</p>\n");
+	    	}	    	
+	    	out.write("</div>\n");
+	    	if(next == null) break;
+	    	max -= 1;
 	    }
+	    out.write("</div>\n</body>\n</html>\n");
 		} catch(Exception ex) {
-			System.out.println("Exception caught: " + ex.toString());
-		}
-		finally {
-			if(out != null) {
-				out.close();
-			}
+			throw ex;
+		} finally {
+			if(scanner != null) scanner.close();
+			if(out != null) out.close();
 		}
 	}
 	
@@ -46,8 +66,14 @@ public class App {
 		return Jsoup.parse(content);		
 	}
 	
-	public static String getBodyFromWuxia(Document doc) {
-		return doc.getElementsByClass("fr-view").get(0).toString();
+	public static List<String> getBodyFromWuxia(Document doc) {
+		return doc.select(".fr-view > p").stream()
+				.map(element -> element.text())
+				.collect(Collectors.toList());
+	}
+	
+	public static String getTitleFromWuxia(Document doc) {
+		return doc.select(".p-15 div > h4").get(0).text();
 	}
 	
 	public static String getNextChapterFromWuxia(Document doc) {
