9f01382e02ae607273c60bf1cf72959ef9c4b057
Ravi Upadhyay <ravupad@gmail.com>
2018-08-28 10:03:30 +0530

    writing to markdown and added wuxia as source interface implmentation

--- src/main/java/rws/App.java
+++ src/main/java/rws/App.java
@@ -3,83 +3,149 @@ package rws;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class App {
-	public static final String ua = 
-			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
-	public static final String wuxia_base = "https://www.wuxiaworld.com";
-	
 	public static void main(String[] args) 
-			throws IOException {
-		Scanner scanner = null;
-		Writer out = null;
-		try {
-			scanner = new Scanner(System.in);
-			System.out.print("Enter url of first chapter: ");
-			String url = scanner.nextLine();
-			System.out.print("Enter max chapters to download: ");
-			long max = Long.parseLong(scanner.nextLine());
+			throws Exception {
+		Scanner scanner = null;	
+		scanner = new Scanner(System.in);
+		System.out.print("Enter url of first chapter: ");
+		String url = scanner.nextLine();
+		System.out.print("Enter max chapters to download: ");
+		var max = Integer.parseInt(scanner.nextLine());
+		WuxiaSource source = new WuxiaSource();
+		var book = new Book(source, url, max);
+		book.writeBook();
+		scanner.close();
+	}
+	
+	public interface Source {
+		String getBaseUrl();
+		String getBookTitle(Document chapter);
+		String getChapterTitle(Document chapter);
+		List<String> getChapterContent(Document chapter);
+		String getNextChapterUrl(Document chapter);
+	}
+	
+	public static class Book {
+		Source source;
+		String url;
+		int max;
+		Document chapter;
+		String book_name;
+		Writer out;		
+		
+		public Book(
+				Source source, 
+				String startUrl, 
+				int max) throws Exception {
+			this.source = source;
+			this.url = startUrl;
+			this.max = max;	
+			chapter = getDocument();
+			book_name = source.getBookTitle(chapter);
 			out = new BufferedWriter(
 					new OutputStreamWriter(
-							new FileOutputStream("test.html"), 
+							new FileOutputStream(book_name + ".md"), 
 							"UTF-8"));
-			String next = null;
-			out.write("<html>\n<head><meta charset='utf-8'></head>\n<body>\n<div class='book'>\n");
-	    while(max > 0) {
-	    	var doc = getDocument(url);
-	    	var title = getTitleFromWuxia(doc);
-	    	var lines = getBodyFromWuxia(doc);
-	    	next = getNextChapterFromWuxia(doc);
-	    	url = wuxia_base + next;
-	    	out.write("<div class='chapter'>\n");
-	    	out.write("<h4>" + title + "</h4>\n");
-	    	for(String line: lines) {
-	    		out.write("<p>" + line + "</p>\n");
-	    	}	    	
-	    	out.write("</div>\n");
-	    	if(next == null) break;
-	    	max -= 1;
-	    }
-	    out.write("</div>\n</body>\n</html>\n");
-		} catch(Exception ex) {
-			throw ex;
-		} finally {
-			if(scanner != null) scanner.close();
-			if(out != null) out.close();
+		}
+		
+		public void writeBook() throws Exception {
+			while(max > 0) {
+				max -= 1;
+				writeChapter();
+				if(!getNextChapter()) return;				
+			}
+			out.close();
+		}
+		
+		void writeChapter() throws Exception {
+			writeChapterTitle();
+			out.write("\n");
+			writeChapterContent();
+			out.write("\n");
+		}
+		
+		void writeChapterTitle() throws Exception {
+			var title = source.getChapterTitle(chapter);
+			out.write(title);
+			out.write("\n");
+			for(int i = 0; i < title.length(); i++) {
+				out.write("=");
+			}
+			out.write("\n");
+		}
+		
+		void writeChapterContent() throws Exception {
+			var paras = source.getChapterContent(chapter);
+			for(var para: paras) {
+				out.write(para);
+				out.write("\n\n");
+			}
+		}
+		
+		boolean getNextChapter() throws Exception {
+			String next = source.getNextChapterUrl(chapter);
+			if(next == null) return false;
+			url = source.getBaseUrl() + next;
+			chapter = getDocument();
+			return true;
+		}
+		
+		Document getDocument() 
+				throws Exception {
+			String ua = 
+					"Mozilla/5.0 "
+					+ "(X11; Linux x86_64) "
+					+ "AppleWebKit/537.36 "
+					+ "(KHTML, like Gecko) "
+					+ "Chrome/68.0.3440.106 "
+					+ "Safari/537.36";
+			var content = Request.Get(url)
+					.setHeader("User-Agent", ua)
+					.execute().returnContent().asString();
+			return Jsoup.parse(content);		
 		}
 	}
 	
-	public static Document getDocument(String url) 
-			throws ClientProtocolException, IOException {
-		var content = Request.Get(url)
-				.setHeader("User-Agent", ua)
-				.execute().returnContent().asString();
-		return Jsoup.parse(content);		
-	}
-	
-	public static List<String> getBodyFromWuxia(Document doc) {
-		return doc.select(".fr-view > p").stream()
-				.map(element -> element.text())
-				.collect(Collectors.toList());
-	}
-	
-	public static String getTitleFromWuxia(Document doc) {
-		return doc.select(".p-15 div > h4").get(0).text();
-	}
-	
-	public static String getNextChapterFromWuxia(Document doc) {
-		var next = doc.select("ul.list-inline > li.next > a").get(0).attr("href");
-		if(next.equals("#")) {
-			return null;
-		} else return next;			
-	}
+	public static class WuxiaSource implements Source {
+
+		@Override
+		public String getBaseUrl() {
+			return "https://www.wuxiaworld.com";
+		}
+
+		@Override
+		public String getBookTitle(Document chapter) {
+			return chapter.select("li.caption a h4").get(0).text();
+		}
+
+		@Override
+		public String getChapterTitle(Document chapter) {
+			return chapter.select(".p-15 div > h4").get(0).text();
+		}
+
+		@Override
+		public List<String> getChapterContent(Document chapter) {
+			return chapter.select(".fr-view > p").stream()
+					.map(element -> element.text())
+					.filter(text -> text.length() > 0)
+					.collect(Collectors.toList());
+		}
+
+		@Override
+		public String getNextChapterUrl(Document chapter) {
+			var next = chapter.select("ul.list-inline > li.next > a").get(0).attr("href");
+			if(next.equals("#")) {
+				return null;
+			} else return next;		
+		}		
+	}			
 }
