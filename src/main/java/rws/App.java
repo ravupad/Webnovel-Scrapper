13b5e5ac0326c211b9f3a4e5e40d1cb429da9838
Ravi Upadhyay <ravupad@gmail.com>
2018-08-29 10:29:41 +0530

    building a single http client for use which can store cookie and has user agent

--- src/main/java/rws/App.java
+++ src/main/java/rws/App.java
@@ -1,6 +1,26 @@
 package rws;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.HttpClients;
+
 public class App {
+	public static HttpClient http_client;
+
+	public static HttpClient getClient() {
+		if (http_client == null) {
+			http_client = HttpClients.custom()
+					.setDefaultCookieStore(new BasicCookieStore())
+					.setUserAgent("Mozilla/5.0 Firefox/26.0")
+					.setDefaultRequestConfig(RequestConfig.custom()
+							.setCookieSpec(CookieSpecs.STANDARD).build())
+					.build();
+		}
+		return http_client;
+	}
+	
 	public static void main(String[] args) 
 			throws Exception {
 		Handler handler = new Handler();
@@ -9,5 +29,4 @@ public class App {
 		handler.selectChapter();
 		handler.fetchChapters();
 	}
-	 
 }
--- src/main/java/rws/wuxiaworld/WuxiaSource.java
+++ src/main/java/rws/wuxiaworld/WuxiaSource.java
@@ -2,24 +2,26 @@ package rws.wuxiaworld;
 
 import java.util.Arrays;
 import java.util.List;
-import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import rws.App;
 import rws.Book;
 import rws.Source;
 
 public class WuxiaSource implements Source {
 	public static final String base_url = "https://www.wuxiaworld.com";
 
-	Document chapter;
-	
+	Document chapter;	
+
 	@Override
 	public List<Book> searchBook(String name) {
 		chapter = getDocument(name);
 		var book = new WuxiaBook(chapter);
 		return Arrays.asList(book);
 	}
-	
+
 	@Override
 	public Book selectBook(String selection) {
 		return new WuxiaBook(chapter);
@@ -27,11 +29,12 @@ public class WuxiaSource implements Source {
 
 	public static Document getDocument(String url) {
 		try {
-			String ua = "Mozilla/5.0 " + "(X11; Linux x86_64) "
-					+ "AppleWebKit/537.36 " + "(KHTML, like Gecko) "
-					+ "Chrome/68.0.3440.106 " + "Safari/537.36";
-			var content = Request.Get(url).setHeader("User-Agent", ua).execute()
-					.returnContent().asString();
+			HttpGet request = new HttpGet(url);
+			var response = App.getClient().execute(request);
+			if (response.getStatusLine().getStatusCode() != 200) {
+				return null;
+			}
+			var content = EntityUtils.toString(response.getEntity(), "UTF-8");
 			return Jsoup.parse(content);
 		} catch (Exception ex) {
 			return null;
